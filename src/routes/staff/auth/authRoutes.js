88fa5a7a22c537d184bd83162609516926729217
bijithkk <informbijith@gmail.com>
2025-07-15 23:58:20 +0530

    removed signup

--- src/routes/staff/auth/authRoutes.js
+++ src/routes/staff/auth/authRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { signup, login } from '../../../controllers/staff/auth/authController.js';
+import { login } from '../../../controllers/staff/auth/authController.js';
 import lowercaseBody from '../../../middleware/lowercaseBody.js';
 
 const router = express.Router();
-
-router.post('/signup', lowercaseBody(['email']), signup);
 
 router.post('/login', lowercaseBody(['email']), login);
 
